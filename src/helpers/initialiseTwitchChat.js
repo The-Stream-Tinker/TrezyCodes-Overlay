3459ce956c570d0c57f35951bd1b545ae951bf1a
Trezy <t@trezy.com>
2022-10-19 14:36:34 -0500

    handle chat actions

--- src/helpers/initialiseTwitchChat.js
+++ src/helpers/initialiseTwitchChat.js
@@ -18,6 +18,36 @@ const twitchChatClient = new ChatClient({})
 
 
 
+async function handleAction(...args) {
+	const [
+		/* channel */,
+		/* username */,
+		/* message */,
+		messageObject,
+	] = args
+
+	messageObject.content.value = (
+		<em>
+			{messageObject.content.value.replace(/.$/u, '').replace(/^.ACTION/u, '')}
+		</em>
+	)
+
+	handleMessage(...args)
+}
+
+async function handleMessage(...args) {
+	const [
+		/* channel */,
+		/* username */,
+		/* message */,
+		messageObject,
+	] = args
+
+	const { addChatMessage } = store.getState()
+
+	addChatMessage(messageObject)
+}
+
 /**
  * Initialises a connection to the Twitch messaging interface and stores messages in the Zustand store.
  */
@@ -28,18 +58,8 @@ export async function initialiseTwitchChat() {
 		isChatConnecting: false,
 	})
 
-	twitchChatClient.onMessage(async (...args) => {
-		const [
-			/* channel */,
-			/* username */,
-			/* message */,
-			messageObject,
-		] = args
-
-		const { addChatMessage } = store.getState()
-
-		addChatMessage(messageObject)
-	})
+	twitchChatClient.onAction(handleAction)
+	twitchChatClient.onMessage(handleMessage)
 
 	await twitchChatClient.onRegister(async () => {
 		await twitchChatClient.join('TrezyCodes')
